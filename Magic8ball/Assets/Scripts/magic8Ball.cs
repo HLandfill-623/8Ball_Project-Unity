2e0a3ec51887b0669945f2a6f4d8f2d3b45b7dff
HLandfill-623 <hinc2142@gmail.com>
2025-09-18 18:56:10 -0500

    Misc. Edits

---
+++ Magic8ball/Assets/Scripts/magic8Ball.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magic8Ball : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    private string[] answers = {"Yes", "No", "Without a doubt", "Reply hazy. Ask again",
+            "Don't count on it", "Most likely", "Outlook not good", "Better not tell you now"};
+
+    void Update()
+    {
+        
+    }
+}
